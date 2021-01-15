91d48a97d11dadf9ec47bf61f455192d13e63793
3RpbNkik <mikhail.s.nyc@gmail.com>
2021-01-14 20:53:29 -0800

    add my code

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import requests
+import re
+import time
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def find_all_anonce(string):
+    new_list = re.findall('(?<=<a\ class="_109rpto\ _1anrh0x"\ href="/item/detail/)[\w\W]*?(?=/"\ data-item-index=)', string)
+    return new_list
 
+def open_page(string):
+    referer = ['https://offerup.com/', 'https://www.google.com', 'https://duckduckgo.com/']
+    user_agent = ['User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0'
+    ]
+    headers = {
+        'User-Agent': f'{random.choice(user_agent)}',
+        'From': f'{random.choice(referer)}'  # This is another valid field
+    }
+    response = requests.get(string, headers=headers)
+    return response.text
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def parse_item(string):
+    item_href = re.findall('(?<="seo":\{"page":\{"url":")[\w\W]*?(?=/",")', string)
+    item_name = re.findall('(?<="product":\{"title":")[\w\W]*?(?=",")', string)
+    item_price = re.findall('(?<=\{"price":\{"amount":")[\w\W]*?(?=",")', string)
+    item_location = re.findall('(?<="\ data-name="market-info">)[\w\W]*?(?=</a><span\ class=)', string)
+    item_category = re.findall('(?<=,"topic":\{"id":")[\w\W]*?(?=","title":")', string)
+    item_image = re.findall('(?<="photos":\[\{"images":\{"detail_full":\{"url":")[\w\W]*?(?=","width":)', string)
+    pars_list = {"href":item_href, "name":item_name, "price":item_price, "location":item_location, "category":item_category, "image":item_image}
+    return pars_list
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+new_list = find_all_anonce(open_page("https://offerup.com/explore/k/cars-trucks/"))
+for id in new_list:
+    print(parse_item(open_page(f'https://offerup.com/item/detail/{id}/')))
+    time.sleep(2)
