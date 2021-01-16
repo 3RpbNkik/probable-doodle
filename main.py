e14821534f085220f8650f984f7e69d457ab7240
3RpbNkik <jHbNgR6VQQ6BXk7>
2021-01-16 12:49:04 -0800

    add telegram parse and push in telegram just add your telegram_id_bot

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import random
 from fake_useragent import UserAgent
 
+bot_id = 'telegram_bot_id'
 
 def find_all_anonce(string):
     new_list = re.findall('(?<=<a\ class="_109rpto\ _1anrh0x"\ href="/item/detail/)[\w\W]*?(?=/"\ data-item-index=)', string)
@@ -12,10 +13,7 @@ def find_all_anonce(string):
 def open_page(string):
     referer = ['https://offerup.com/', 'https://www.google.com', 'https://duckduckgo.com/']
     user_agent = random.choice(UserAgent().data_browsers["chrome"])
-    headers = {
-        'User-Agent': f'{user_agent}',
-        'From': f'{random.choice(referer)}'  # This is another valid field
-    }
+    headers = {'User-Agent': f'{user_agent}', 'From': f'{random.choice(referer)}'}
     response = requests.get(string, headers=headers)
     return response.text
 
@@ -29,7 +27,24 @@ def parse_item(string):
     pars_list = {"href":item_href, "name":item_name, "price":item_price, "location":item_location, "category":item_category, "image":item_image}
     return pars_list
 
+# def send_message_telegram(bot_id, method, message):
+#     url = requests.get(f'https://api.telegram.org/bot{bot_id}/{method}/{message}')
+
+def send_photo_and_description(bot_id, image, description):
+    url = 'https://api.telegram.org/bot'
+    method = '/sendPhoto?chat_id=@offer_autoandparts&photo='
+    caption = '&caption='
+    requests.get(f'{url}{bot_id}{method}{image}{caption}{description}')
+
 new_list = find_all_anonce(open_page("https://offerup.com/explore/k/cars-trucks/"))
 for id in new_list:
-    print(parse_item(open_page(f'https://offerup.com/item/detail/{id}/')))
-    time.sleep(2)
+    item_list = parse_item(open_page(f'https://offerup.com/item/detail/{id}/'))
+    href = item_list['href'].pop()
+    name = item_list['name'].pop()
+    price = item_list['price'].pop()[:-3]
+    location = item_list['location'].pop()
+    category = item_list['category'].pop()
+    image = item_list['image'].pop()
+    description = f'{name}  ${price}   {location}    {href}'
+    send_photo_and_description(bot_id, image, description)
+    time.sleep(1)
