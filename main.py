61a32aec540274c68bc7ed56a84087f000e6ab1d
3RpbNkik <jHbNgR6VQQ6BXk7>
2021-01-18 23:47:13 -0800

    add duplicate check checking only by id

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import requests
+from fake_useragent import UserAgent
+import os
 import re
 import csv
 import time
 import random
-from fake_useragent import UserAgent
+import requests
 
 bot_id = 'telegram_bot_id'
 
@@ -29,10 +30,21 @@ def parse_item(string):
     return pars_list
 
 def write_history_file(new_list):
-    print(type(new_list))
     with open('history.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(new_list)
+
+def read_history_file(id):
+    with open('history.csv', 'r', newline='') as f:
+        reader = csv.reader(f)
+        if os.stat('history.csv').st_size == 0:
+            return False
+        else:
+            x = False
+            for row in reader:
+                if id == row[0]:
+                    return True
+            return x
 
 def send_photo_and_description(bot_id, image, description):
     url = 'https://api.telegram.org/bot'
@@ -40,17 +52,24 @@ def send_photo_and_description(bot_id, image, description):
     caption = '&caption='
     requests.get(f'{url}{bot_id}{method}{image}{caption}{description}')
 
-new_list = find_all_anonce(open_page("https://offerup.com/explore/k/cars-trucks/"))
-for id in new_list:
-    item_list = parse_item(open_page(f'https://offerup.com/item/detail/{id}/'))
-    href = str(item_list['href'].pop())
-    name = str(item_list['name'].pop())
-    price = str(item_list['price'].pop()[:-3])
-    location = str(item_list['location'].pop())
-    category = str(item_list['category'].pop())
-    image = str(item_list['image'].pop())
-    csv_line = [id, name, price, location, category, href]
-    description = f'{name}  ${price}   {location}    {href}'
-    write_history_file(csv_line)
-    send_photo_and_description(bot_id, image, description)
-    time.sleep(1)
+def main():
+    new_list = find_all_anonce(open_page("https://offerup.com/explore/k/cars-trucks/"))
+    for id in new_list:
+        if read_history_file(id) == False:
+            item_list = parse_item(open_page(f'https://offerup.com/item/detail/{id}/'))
+            href = item_list['href'].pop()
+            name = item_list['name'].pop()
+            price = item_list['price'].pop()[:-3]
+            location = item_list['location'].pop()
+            category = item_list['category'].pop()
+            image = item_list['image'].pop()
+            csv_line = [id, name, price, location, category, href]
+            description = f'{name}  ${price}   {location}    {href}'
+            write_history_file(csv_line)
+            send_photo_and_description(bot_id, image, description)
+            time.sleep(1)
+    print('Done wait 120s')
+    time.sleep(120)
+    main()
+
+main()
